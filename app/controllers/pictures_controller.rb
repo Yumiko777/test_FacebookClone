294bad35648fd45c59f13c8889133d3c548f65f6
Yumiko <wakinghira73@gmail.com>
2021-05-11 20:18:03 +0900

    css修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -29,37 +29,26 @@ class PicturesController < ApplicationController
   def create
     @picture = Picture.new(picture_params)
     @picture.user_id = current_user.id
-
-    respond_to do |format|
-      if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
-        format.json { render :show, status: :created, location: @picture }
-      else
-        format.html { render :new }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
+    if @picture.save
+      PicturetMailer.picture_mail(@picture).deliver
+      redirect_to pictures_path, notice: "作成しました！"
+    else
+      render : new
     end
   end
 
   def update
-    respond_to do |format|
-      if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
-        format.json { render :show, status: :ok, location: @picture }
-      else
-        format.html { render :edit }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "編集しました！"
+    else
+      render :edit
     end
   end
 
   def destroy
-    @picture.destroy
-    respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+     @picture.destroy
+     redirect_to pictures_path, notice: "削除しました！"
+   end
 
   private
   def picture_params
